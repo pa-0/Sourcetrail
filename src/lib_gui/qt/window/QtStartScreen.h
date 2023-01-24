96837de36ee5a39e5c4fe61e876af74f04d550da
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2023-01-24 20:59:11 +0200

    src: Format QtStartScreen
    ticket: SOUR-33

--- src/lib_gui/qt/window/QtStartScreen.h
+++ src/lib_gui/qt/window/QtStartScreen.h
@@ -1,58 +1,53 @@
-#ifndef QT_START_SCREEN_H
-#define QT_START_SCREEN_H
-
+#pragma once
+// Qt5
 #include <QPushButton>
-
+// Internal
 #include "FilePath.h"
 #include "QtWindow.h"
 
-class QtRecentProjectButton: public QPushButton
-{
-	Q_OBJECT
+class QtRecentProjectButton : public QPushButton {
+  Q_OBJECT
 
 public:
-	QtRecentProjectButton(QWidget* parent);
-	bool projectExists() const;
-	void setProjectPath(const FilePath& projectFilePath);
+  QtRecentProjectButton(QWidget* parent);
+  bool projectExists() const;
+  void setProjectPath(const FilePath& projectFilePath);
 public slots:
-	void handleButtonClick();
+  void handleButtonClick();
+
 signals:
-	void updateButtons();
+  void updateButtons();
 
 private:
-	bool m_projectExists;
-	FilePath m_projectFilePath;
+  bool m_projectExists;
+  FilePath m_projectFilePath;
 };
 
-
-class QtStartScreen: public QtWindow
-{
-	Q_OBJECT
+class QtStartScreen : public QtWindow {
+  Q_OBJECT
 
 public:
-	QtStartScreen(QWidget* parent = 0);
-	QSize sizeHint() const override;
+  QtStartScreen(QWidget* parent = 0);
+  QSize sizeHint() const override;
 
-	void setupStartScreen();
+  void setupStartScreen();
 
 signals:
-	void openOpenProjectDialog();
-	void openNewProjectDialog();
+  void openOpenProjectDialog();
+  void openNewProjectDialog();
 
 private slots:
-	void handleNewProjectButton();
-	void handleOpenProjectButton();
-	void handleRecentButton();
-	void updateButtons();
+  void handleNewProjectButton();
+  void handleOpenProjectButton();
+  void handleRecentButton();
+  void updateButtons();
 
 private:
-	std::vector<QtRecentProjectButton*> m_recentProjectsButtons;
-	const QIcon m_cppIcon;
-	const QIcon m_cIcon;
-	const QIcon m_pythonIcon;
-	const QIcon m_javaIcon;
-	const QIcon m_projectIcon;
-	const QIcon m_githubIcon;
+  std::vector<QtRecentProjectButton*> m_recentProjectsButtons;
+  const QIcon m_cppIcon;
+  const QIcon m_cIcon;
+  const QIcon m_pythonIcon;
+  const QIcon m_javaIcon;
+  const QIcon m_projectIcon;
+  const QIcon m_githubIcon;
 };
-
-#endif	  // QT_START_SCREEN_H
