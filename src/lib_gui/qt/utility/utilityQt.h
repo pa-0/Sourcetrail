d8a0dfbd8ec0e612b8d1cd5511868113062ed2f2
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2023-01-24 23:40:38 +0200

    src: Format utilityQt
    ticket: SOUR-33

--- src/lib_gui/qt/utility/utilityQt.h
+++ src/lib_gui/qt/utility/utilityQt.h
@@ -1,6 +1,5 @@
-#ifndef UTILITY_QT_H
-#define UTILITY_QT_H
-
+#pragma once
+// STL
 #include <string>
 
 class FilePath;
@@ -12,9 +11,10 @@ class QWidget;
 class QtMainWindow;
 class ViewLayout;
 
-namespace utility
-{
+namespace utility {
+
 void setWidgetBackgroundColor(QWidget* widget, const std::string& color);
+
 void setWidgetRetainsSpaceWhenHidden(QWidget* widget);
 
 void loadFontsFromDirectory(const FilePath& path, const std::wstring& extension = L".otf");
@@ -22,11 +22,11 @@ void loadFontsFromDirectory(const FilePath& path, const std::wstring& extension 
 std::string getStyleSheet(const FilePath& path);
 
 QPixmap colorizePixmap(const QPixmap& pixmap, QColor color);
+
 QIcon createButtonIcon(const FilePath& iconPath, const std::string& colorId);
 
 QtMainWindow* getMainWindowforMainView(ViewLayout* viewLayout);
 
 void copyNewFilesFromDirectory(const QString& src, const QString& dst);
-}	 // namespace utility
 
-#endif	  // UTILITY_QT_H
+}    // namespace utility
