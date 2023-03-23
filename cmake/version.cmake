de3f6e8798a86770820f8664fff4194356c46850
Ahmed Hussein <thelastrealdevil@gmail.com>
2023-03-23 16:28:20 +0200

    Ci (#2)
    * Update build.yml
    
    * force to pull all repo so git can work
    
    * Update build.yml
    
    * Update build.yml
    
    * Update build.yml
    
    * build: Format cmake/version
    
    * build: Remove Funcing
    
    * build: Install conan v1
    
    * build: downgrade github action checkout to v2
    
    * build: Stackoverflow solution for git desc problem
    
    * build: Debug git version #1
    
    * build: workaround action/checkout
    
    * Update build.yml
    
    * Update version.cmake
    
    * Add missing submodule flag
    
    * Update build.yml
    
    * build: Fix year.major.commit
    
    * build: Add CLang to github action
    
    * rename github action branch to master
    
    ---------
    
    Co-authored-by: Ahmed Hussein <eng.ahmedhussein89@gmail.com>

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -29,6 +29,50 @@ if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
         OUTPUT_VARIABLE GIT_VERSION_NUMBER
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
+    string(REGEX REPLACE "^([0-9]+)\\..*"              "\\1" VERSION_YEAR   "${GIT_VERSION_NUMBER}")
+    string(REGEX REPLACE "^[0-9]+\\.([0-9]+).*"        "\\1" VERSION_MINOR  "${GIT_VERSION_NUMBER}")
+    string(REGEX REPLACE "^[0-9]+\\.[0-9]+-([0-9]+).*" "\\1" VERSION_COMMIT "${GIT_VERSION_NUMBER}")
+else()
+    set(GIT_BRANCH         "")
+    set(GIT_COMMIT_HASH    "")
+    set(GIT_VERSION_NUMBER "")
+    set(VERSION_YEAR       "0")
+    set(VERSION_MINOR      "0")
+    set(VERSION_COMMIT     "0")
+    set(BUILD_TYPE         "")
+endif()
+
+# simple check for a git repo
+if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
+	find_package(Git)
+
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_BRANCH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} log -1 --format=%h
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_COMMIT_HASH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} log -1 --format=%ci
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_COMMIT_TIME
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} describe --long --match "[0-9]*" HEAD
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_VERSION_NUMBER
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
     string(REGEX REPLACE "^([0-9]+)\\..*" "\\1" VERSION_YEAR "${GIT_VERSION_NUMBER}")
     string(REGEX REPLACE "^[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${GIT_VERSION_NUMBER}")
     string(REGEX REPLACE "^[0-9]+\\.[0-9]+-([0-9]+).*" "\\1" VERSION_COMMIT "${GIT_VERSION_NUMBER}")
@@ -45,13 +89,21 @@ else(EXISTS "${CMAKE_SOURCE_DIR}/.git")
 endif(EXISTS "${CMAKE_SOURCE_DIR}/.git")
 
 set(VERSION_STRING "${VERSION_YEAR}.${VERSION_MINOR}.${VERSION_COMMIT}")
+if("${VERSION_STRING}" STREQUAL "..")
+    # TODO(Hussein): For texting
+    set(VERSION_YEAR "2023")
+    set(VERSION_MINOR "1")
+    set(VERSION_COMMIT "126")
+    set(VERSION_STRING "2023.1.126")
+endif()
 
 message(STATUS "Version: ${VERSION_STRING}")
 
-# message(STATUS "Git current branch: ${GIT_BRANCH}")
-# message(STATUS "Git version number: " ${GIT_VERSION_NUMBER} )
-# message(STATUS "Git commit hash: ${GIT_COMMIT_HASH}")
-# message(STATUS "Git commit time: ${GIT_COMMIT_TIME}")
-# message(STATUS "Version year: ${VERSION_YEAR}")
-# message(STATUS "Version minor: ${VERSION_MINOR}")
-# message(STATUS "Version commit: ${VERSION_COMMIT}")
+message(STATUS "Version: ${VERSION_STRING}")
+message(STATUS "Git current branch: ${GIT_BRANCH}")
+message(STATUS "Git version number: " ${GIT_VERSION_NUMBER} )
+message(STATUS "Git commit hash: ${GIT_COMMIT_HASH}")
+message(STATUS "Git commit time: ${GIT_COMMIT_TIME}")
+message(STATUS "Version year: ${VERSION_YEAR}")
+message(STATUS "Version minor: ${VERSION_MINOR}")
+message(STATUS "Version commit: ${VERSION_COMMIT}")
