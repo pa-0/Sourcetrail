9cd4ab8d86dfc60684954051c7a9b80d5b74c59e
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2023-05-05 12:43:49 +0300

    src: Format QtWindowStack

--- src/lib_gui/qt/window/QtWindowStack.h
+++ src/lib_gui/qt/window/QtWindowStack.h
@@ -1,47 +1,44 @@
-#ifndef QT_WINDOW_STACK
-#define QT_WINDOW_STACK
-
+#pragma once
+// STL
 #include <vector>
-
+// Qt5
 #include <QWidget>
 
-class QtWindowStackElement: public QWidget
-{
-	Q_OBJECT
+class QtWindowStackElement : public QWidget {
+  // NOLINTNEXTLINE
+  Q_OBJECT
 public:
-	QtWindowStackElement(QWidget* parent = nullptr);
+  explicit QtWindowStackElement(QWidget* parent = nullptr);
 
-	virtual void showWindow() = 0;
-	virtual void hideWindow() = 0;
+  virtual void showWindow() = 0;
+  virtual void hideWindow() = 0;
 };
 
 
-class QtWindowStack: public QObject
-{
-	Q_OBJECT
+class QtWindowStack : public QObject {
+  // NOLINTNEXTLINE
+  Q_OBJECT
 
 signals:
-	void empty();
-	void pop();
-	void push();
+  void empty();
+  void pop();
+  void push();
 
 public:
-	QtWindowStack(QObject* parent = nullptr);
+  explicit QtWindowStack(QObject* parent = nullptr);
 
-	QtWindowStackElement* getTopWindow() const;
-	QtWindowStackElement* getBottomWindow() const;
+  [[nodiscard]] QtWindowStackElement* getTopWindow() const;
+  [[nodiscard]] QtWindowStackElement* getBottomWindow() const;
 
-	size_t getWindowCount() const;
+  [[nodiscard]] size_t getWindowCount() const;
 
-	void centerSubWindows();
+  void centerSubWindows();
 
 public slots:
-	void pushWindow(QtWindowStackElement* window);
-	void popWindow();
-	void clearWindows();
+  void pushWindow(QtWindowStackElement* window);
+  void popWindow();
+  void clearWindows();
 
 private:
-	std::vector<QtWindowStackElement*> m_stack;
+  std::vector<QtWindowStackElement*> m_stack;
 };
-
-#endif	  // QT_WINDOW_STACK
