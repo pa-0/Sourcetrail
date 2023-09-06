2093e8a461e5132d4804674713624efc5abf3cb7
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2023-09-06 20:07:30 +0300

    src: Add a test for AppPath

---
+++ src/test/AppPathTestSuite.cpp
@@ -0,0 +1,27 @@
+#include <filesystem>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "AppPath.h"
+
+TEST(AppPath, CxxIndexer) {
+#ifdef D_WINDOWS
+  const auto exec = "sourcetrail_indexer.exe"
+#else
+  const auto exec = "sourcetrail_indexer";
+#endif
+
+  EXPECT_EQ(exec, AppPath::getCxxIndexerFilePath().str());
+  EXPECT_FALSE(AppPath::setCxxIndexerDirectoryPath(FilePath{""}));
+
+  EXPECT_FALSE(AppPath::setSharedDataDirectoryPath(FilePath{""}));
+  EXPECT_EQ("", AppPath::getSharedDataDirectoryPath().str());
+  const auto tempDir = std::filesystem::temp_directory_path();
+  EXPECT_TRUE(AppPath::setSharedDataDirectoryPath(FilePath{tempDir / "shared"}));
+  EXPECT_EQ(tempDir / "shared", AppPath::getSharedDataDirectoryPath().str());
+  EXPECT_EQ(tempDir / "shared" / exec, AppPath::getCxxIndexerFilePath().str());
+
+  EXPECT_TRUE(AppPath::setCxxIndexerDirectoryPath(FilePath{tempDir}));
+  EXPECT_EQ(tempDir / exec, AppPath::getCxxIndexerFilePath().str());
+}
