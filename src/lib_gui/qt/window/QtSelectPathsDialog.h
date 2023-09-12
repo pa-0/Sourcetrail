89216bd741e5cb5993e50f74c0b95279df2f86d7
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2023-09-12 22:29:48 +0300

    src: Fix PVS for QtSelectPathsDialog

--- src/lib_gui/qt/window/QtSelectPathsDialog.h
+++ src/lib_gui/qt/window/QtSelectPathsDialog.h
@@ -1,30 +1,25 @@
-#ifndef QT_SELECT_PATHS_DIALOG_H
-#define QT_SELECT_PATHS_DIALOG_H
+#pragma once
 
 #include "QtTextEditDialog.h"
 
 class FilePath;
 class QListWidget;
 
-class QtSelectPathsDialog: public QtTextEditDialog
-{
-	Q_OBJECT
+class QtSelectPathsDialog final : public QtTextEditDialog {
+  Q_OBJECT
 public:
-	QtSelectPathsDialog(const QString& title, const QString& description, QWidget* parent = 0);
+  QtSelectPathsDialog(const QString& title, const QString& description, QWidget* parent = 0);
 
-	std::vector<FilePath> getPathsList() const;
-	void setPathsList(
-		const std::vector<FilePath>& paths,
-		const std::vector<FilePath>& checkedPaths,
-		const FilePath& rootPathForRelativePaths);
+  [[nodiscard]] std::vector<FilePath> getPathsList() const;
+  void setPathsList(const std::vector<FilePath>& paths,
+                    const std::vector<FilePath>& checkedPaths,
+                    const FilePath& rootPathForRelativePaths);
 
-	virtual void populateWindow(QWidget* widget) override;
-	virtual void windowReady() override;
+  void populateWindow(QWidget* widget) override;
+  void windowReady() override;
 
 private:
-	QListWidget* m_list;
+  QListWidget* m_list = nullptr;
 
-	void checkSelected(bool checked);
+  void checkSelected(bool checked);
 };
-
-#endif	  // QT_SELECT_PATHS_DIALOG_H
