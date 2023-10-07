a56c3720418df46dd1dfe4fd5fc887fddde51266
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2023-10-07 21:30:17 +0300

    src: Move globalStringsTestSuite to GTest

--- src/lib_gui/tests/globalStringsTestSuite.cpp
+++ src/lib_gui/tests/globalStringsTestSuite.cpp
@@ -1,5 +1,5 @@
-// catch2
-#include <catch2/catch_all.hpp>
+// gtest
+#include <gtest/gtest.h>
 // internal
 #include "globalStrings.h"
 
@@ -8,8 +8,8 @@ constexpr bool strings_equal(char const* value0, char const* value1) {
 }
 
 // NOLINTNEXTLINE(cert-err58-cpp)
-TEST_CASE("get github url", "[lib_gui]") {
+TEST(getGithubUrl, goodCase) {
   constexpr auto URL = "github"_g;
-  STATIC_CHECK(std::is_same_v<const char* const, decltype(URL)>);
-  STATIC_CHECK(strings_equal(URL, "https://github.com/OpenSourceSourceTrail/Sourcetrail"));
+  static_assert( std::is_same_v<const char* const, decltype(URL)>);
+  EXPECT_EQ(URL, "https://github.com/OpenSourceSourceTrail/Sourcetrail");
 }
