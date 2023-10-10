1e8b1a94a5776ccc43741cbd0b201b1948ae2fa1
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2023-10-10 22:20:38 +0300

    src: Add a mock for StatusBarView

---
+++ src/lib/tests/mocks/MockedStatusBarView.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <gmock/gmock.h>
+
+#include "StatusBarView.h"
+
+struct MockedStatusBarView : StatusBarView {
+  MockedStatusBarView(ViewLayout* viewLayout) : StatusBarView(viewLayout) {}
+  MOCK_METHOD(void, createWidgetWrapper, (), (override));
+  MOCK_METHOD(void, refreshView, (), (override));
+  MOCK_METHOD(void, showMessage, (const std::wstring&,bool,bool), (override));
+  MOCK_METHOD(void, setErrorCount, (ErrorCountInfo), (override));
+  MOCK_METHOD(void, showIdeStatus, (const std::wstring&), (override));
+  MOCK_METHOD(void, showIndexingProgress, (size_t), (override));
+  MOCK_METHOD(void, hideIndexingProgress, (), (override));
+};
