5e4d5458709acc98507e4a3a41a79e6023c6bafd
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2023-10-12 19:55:11 +0300

    src: Format QtStatusBarView

--- src/lib_gui/qt/view/QtStatusBarView.cpp
+++ src/lib_gui/qt/view/QtStatusBarView.cpp
@@ -3,40 +3,42 @@
 #include <QStatusBar>
 
 #include "QtMainView.h"
+#include "QtStatusBar.h"
+#include "logging.h"
 
-QtStatusBarView::QtStatusBarView(ViewLayout* viewLayout): StatusBarView(viewLayout)
-{
-	m_widget = new QtStatusBar();
-	m_widget->show();
+QtStatusBarView::QtStatusBarView(ViewLayout* viewLayout) : StatusBarView(viewLayout), m_widget(new QtStatusBar) {
+  m_widget->show();
 
-	dynamic_cast<QtMainView*>(viewLayout)->setStatusBar(m_widget);
+  // TODO(Hussein): Find anther way to set StatusBar
+  if(auto* mainView = dynamic_cast<QtMainView*>(viewLayout); mainView != nullptr) {
+    mainView->setStatusBar(m_widget);
+  } else {
+    LOG_WARNING("Can't cast 'viewlayout' to 'QtMainView'");
+  }
 }
+
+QtStatusBarView::~QtStatusBarView() = default;
 
 void QtStatusBarView::createWidgetWrapper() {}
 
 void QtStatusBarView::refreshView() {}
 
-void QtStatusBarView::showMessage(const std::wstring& message, bool isError, bool showLoader)
-{
-	m_onQtThread([=]() { m_widget->setText(message, isError, showLoader); });
+void QtStatusBarView::showMessage(const std::wstring& message, bool isError, bool showLoader) {
+  m_onQtThread([=]() { m_widget->setText(message, isError, showLoader); });
 }
 
-void QtStatusBarView::setErrorCount(ErrorCountInfo errorCount)
-{
-	m_onQtThread([=]() { m_widget->setErrorCount(errorCount); });
+void QtStatusBarView::setErrorCount(ErrorCountInfo errorCount) {
+  m_onQtThread([=]() { m_widget->setErrorCount(errorCount); });
 }
 
-void QtStatusBarView::showIdeStatus(const std::wstring& message)
-{
-	m_onQtThread([=]() { m_widget->setIdeStatus(message); });
+void QtStatusBarView::showIdeStatus(const std::wstring& message) {
+  m_onQtThread([=]() { m_widget->setIdeStatus(message); });
 }
 
-void QtStatusBarView::showIndexingProgress(size_t progressPercent)
-{
-	m_onQtThread([=]() { m_widget->showIndexingProgress(progressPercent); });
+void QtStatusBarView::showIndexingProgress(size_t progressPercent) {
+  m_onQtThread([=]() { m_widget->showIndexingProgress(progressPercent); });
 }
 
-void QtStatusBarView::hideIndexingProgress()
-{
-	m_onQtThread([=]() { m_widget->hideIndexingProgress(); });
+void QtStatusBarView::hideIndexingProgress() {
+  m_onQtThread([=]() { m_widget->hideIndexingProgress(); });
 }
--- src/lib_gui/qt/view/QtStatusBarView.h
+++ src/lib_gui/qt/view/QtStatusBarView.h
@@ -1,37 +1,34 @@
-#ifndef QT_STATUS_BAR_VIEW_H
-#define QT_STATUS_BAR_VIEW_H
+#pragma once
 
 #include <memory>
 #include <string>
 
 #include "ErrorCountInfo.h"
-#include "QtStatusBar.h"
 #include "QtThreadedFunctor.h"
 #include "StatusBarView.h"
 
-class QtStatusBarView: public StatusBarView
-{
+class QtStatusBar;
+
+class QtStatusBarView : public StatusBarView {
 public:
-	QtStatusBarView(ViewLayout* viewLayout);
-	~QtStatusBarView() = default;
+  explicit QtStatusBarView(ViewLayout* viewLayout);
+  ~QtStatusBarView() override;
 
-	// View implementation
-	void createWidgetWrapper() override;
-	void refreshView() override;
+  // View implementation
+  void createWidgetWrapper() override;
+  void refreshView() override;
 
-	// StatusBar view implementation
-	void showMessage(const std::wstring& message, bool isError, bool showLoader) override;
-	void setErrorCount(ErrorCountInfo errorCount) override;
+  // StatusBar view implementation
+  void showMessage(const std::wstring& message, bool isError, bool showLoader) override;
+  void setErrorCount(ErrorCountInfo errorCount) override;
 
-	void showIdeStatus(const std::wstring& message) override;
+  void showIdeStatus(const std::wstring& message) override;
 
-	void showIndexingProgress(size_t progressPercent) override;
-	void hideIndexingProgress() override;
+  void showIndexingProgress(size_t progressPercent) override;
+  void hideIndexingProgress() override;
 
 private:
-	QtThreadedLambdaFunctor m_onQtThread;
+  QtThreadedLambdaFunctor m_onQtThread;
 
-	QtStatusBar* m_widget;
+  QtStatusBar* m_widget;
 };
-
-#endif	  // !QT_STATUS_BAR_VIEW_H
