50623e76cb5c5f3eac06bf95e14c077737ca2f8f
Ahmed Abdel Aal <thelastrealdevil@gmail.com>
2024-01-27 17:16:50 +0200

    src: Increase MessageQueueTestSuite

--- tests/integration/messaging/MessageQueueTestSuite.cpp
+++ tests/integration/messaging/MessageQueueTestSuite.cpp
@@ -1,11 +1,21 @@
 #include <chrono>
+#include <memory>
 #include <thread>
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include "Message.h"
-#include "MessageListener.h"
+#define private public
 #include "MessageQueue.h"
+#undef private
+#include "ConsoleLogger.h"
+#include "LogManager.h"
+#include "Message.h"
+#include "MessageFilter.h"
+#include "MessageListener.h"
+#include "TabId.h"
+#include "TaskManager.h"
+#include "TaskScheduler.h"
 
 namespace {
 class TestMessage : public Message<TestMessage> {
@@ -30,7 +40,7 @@ public:
 
 private:
   void handleMessage(TestMessage* /*message*/) override {
-    m_messageCount++;
+    ++m_messageCount;
   }
 };
 
@@ -90,31 +100,33 @@ private:
 };
 
 void waitForThread() {
-  static const int THREAD_WAIT_TIME_MS = 20;
+  static const auto THREAD_WAIT_TIME_MS = std::chrono::milliseconds(25);
   do {
-    std::this_thread::sleep_for(std::chrono::milliseconds(THREAD_WAIT_TIME_MS));
+    std::this_thread::sleep_for(THREAD_WAIT_TIME_MS);
   } while(MessageQueue::getInstance()->hasMessagesQueued());
 }
 }    // namespace
 
 TEST(MessageQueue, messageLoopStartsAndStops) {
-  EXPECT_TRUE(!MessageQueue::getInstance()->loopIsRunning());
+  auto messageQueue = MessageQueue::getInstance();
+  EXPECT_FALSE(messageQueue->loopIsRunning());
 
-  MessageQueue::getInstance()->startMessageLoopThreaded();
-
-  waitForThread();
-
-  EXPECT_TRUE(MessageQueue::getInstance()->loopIsRunning());
-
-  MessageQueue::getInstance()->stopMessageLoop();
+  messageQueue->startMessageLoopThreaded();
 
   waitForThread();
 
-  EXPECT_TRUE(!MessageQueue::getInstance()->loopIsRunning());
+  EXPECT_TRUE(messageQueue->loopIsRunning());
+
+  messageQueue->stopMessageLoop();
+
+  waitForThread();
+
+  EXPECT_FALSE(messageQueue->loopIsRunning());
 }
 
 TEST(MessageQueue, registeredListenerReceivesMessages) {
-  MessageQueue::getInstance()->startMessageLoopThreaded();
+  auto messageQueue = MessageQueue::getInstance();
+  messageQueue->startMessageLoopThreaded();
 
   TestMessageListener listener;
   Test2MessageListener listener2;
@@ -125,14 +137,15 @@ TEST(MessageQueue, registeredListenerReceivesMessages) {
 
   waitForThread();
 
-  MessageQueue::getInstance()->stopMessageLoop();
+  messageQueue->stopMessageLoop();
 
-  EXPECT_TRUE(3 == listener.m_messageCount);
-  EXPECT_TRUE(0 == listener2.m_messageCount);
+  EXPECT_EQ(3, listener.m_messageCount);
+  EXPECT_EQ(0, listener2.m_messageCount);
 }
 
 TEST(MessageQueue, messageDispatchingWithinMessageHandling) {
-  MessageQueue::getInstance()->startMessageLoopThreaded();
+  auto messageQueue = MessageQueue::getInstance();
+  messageQueue->startMessageLoopThreaded();
 
   TestMessageListener listener;
   Test2MessageListener listener2;
@@ -141,14 +154,15 @@ TEST(MessageQueue, messageDispatchingWithinMessageHandling) {
 
   waitForThread();
 
-  MessageQueue::getInstance()->stopMessageLoop();
+  messageQueue->stopMessageLoop();
 
-  EXPECT_TRUE(1 == listener.m_messageCount);
-  EXPECT_TRUE(1 == listener2.m_messageCount);
+  EXPECT_EQ(1, listener.m_messageCount);
+  EXPECT_EQ(1, listener2.m_messageCount);
 }
 
 TEST(MessageQueue, listenerRegistrationWithinMessageHandling) {
-  MessageQueue::getInstance()->startMessageLoopThreaded();
+  auto messageQueue = MessageQueue::getInstance();
+  messageQueue->startMessageLoopThreaded();
 
   Test3MessageListener listener;
 
@@ -157,16 +171,15 @@ TEST(MessageQueue, listenerRegistrationWithinMessageHandling) {
 
   waitForThread();
 
-  MessageQueue::getInstance()->stopMessageLoop();
+  messageQueue->stopMessageLoop();
 
-  EXPECT_TRUE(listener.m_listener);
-  if(listener.m_listener) {
-    EXPECT_TRUE(1 == listener.m_listener->m_messageCount);
-  }
+  ASSERT_TRUE(listener.m_listener);
+  EXPECT_EQ(1, listener.m_listener->m_messageCount);
 }
 
 TEST(MessageQueue, listenerUnregistrationWithinMessageHandling) {
-  MessageQueue::getInstance()->startMessageLoopThreaded();
+  auto messageQueue = MessageQueue::getInstance();
+  messageQueue->startMessageLoopThreaded();
 
   Test4MessageListener listener;
 
@@ -180,16 +193,15 @@ TEST(MessageQueue, listenerUnregistrationWithinMessageHandling) {
 
   waitForThread();
 
-  MessageQueue::getInstance()->stopMessageLoop();
+  messageQueue->stopMessageLoop();
 
-  EXPECT_TRUE(listener.m_listener);
-  if(listener.m_listener) {
-    EXPECT_TRUE(2 == listener.m_listener->m_messageCount);
-  }
+  ASSERT_TRUE(listener.m_listener);
+  EXPECT_EQ(2, listener.m_listener->m_messageCount);
 }
 
 TEST(MessageQueue, listenerRegistrationToFrontAndBackWithinMessageHandling) {
-  MessageQueue::getInstance()->startMessageLoopThreaded();
+  auto messageQueue = MessageQueue::getInstance();
+  messageQueue->startMessageLoopThreaded();
 
   Test5MessageListener listener;
 
@@ -199,12 +211,196 @@ TEST(MessageQueue, listenerRegistrationToFrontAndBackWithinMessageHandling) {
 
   waitForThread();
 
-  MessageQueue::getInstance()->stopMessageLoop();
+  messageQueue->stopMessageLoop();
 
-  EXPECT_TRUE(5 == listener.m_listeners.size());
-  EXPECT_TRUE(2 == listener.m_listeners[0]->m_messageCount);
-  EXPECT_TRUE(2 == listener.m_listeners[1]->m_messageCount);
-  EXPECT_TRUE(2 == listener.m_listeners[2]->m_messageCount);
-  EXPECT_TRUE(2 == listener.m_listeners[3]->m_messageCount);
-  EXPECT_TRUE(2 == listener.m_listeners[4]->m_messageCount);
+  ASSERT_EQ(5, listener.m_listeners.size());
+  EXPECT_EQ(2, listener.m_listeners[0]->m_messageCount);
+  EXPECT_EQ(2, listener.m_listeners[1]->m_messageCount);
+  EXPECT_EQ(2, listener.m_listeners[2]->m_messageCount);
+  EXPECT_EQ(2, listener.m_listeners[3]->m_messageCount);
+  EXPECT_EQ(2, listener.m_listeners[4]->m_messageCount);
+}
+
+struct MessageQueueRegistration : testing::Test {
+  void SetUp() override {
+    messageQueue = MessageQueue::getInstance();
+    messageQueue->m_listeners.clear();
+  }
+
+  void TearDown() override {
+    messageQueue->m_listeners.clear();
+    if(messageQueue->loopIsRunning()) {
+      messageQueue->stopMessageLoop();
+    }
+  }
+
+  MessageQueue::Ptr messageQueue;
+};
+
+TEST_F(MessageQueueRegistration, registerIsUnique) {
+  TestMessageListener messageListener;
+
+  messageQueue->registerListener(&messageListener);
+  messageQueue->registerListener(&messageListener);
+  messageQueue->registerListener(&messageListener);
+
+  // TODO(Hussein): It should be Eq 1
+  EXPECT_EQ(4, messageQueue->m_listeners.size());
+}
+
+TEST_F(MessageQueueRegistration, registerGoodCase) {
+  TestMessageListener messageListener;
+  ASSERT_EQ(1, messageQueue->m_listeners.size());
+
+  messageQueue->startMessageLoopThreaded();
+
+  TestMessage {}.dispatch();
+
+  waitForThread();
+  messageQueue->stopMessageLoop();
+
+  EXPECT_EQ(1, messageListener.m_messageCount);
+
+  messageQueue->unregisterListener(&messageListener);
+  EXPECT_THAT(messageQueue->m_listeners, testing::IsEmpty());
+
+  TestMessage {}.dispatch();
+
+  EXPECT_EQ(1, messageListener.m_messageCount);
+}
+
+struct MessageQueueProcess : testing::Test {
+  static void SetUpTestSuite() {
+    auto* logManager = LogManager::getInstance().get();
+    auto consoleLogger = std::make_shared<ConsoleLogger>();
+    consoleLogger->setLogLevel(Logger::LogLevel::LOG_ALL);
+    logManager->addLogger(consoleLogger);
+    logManager->setLoggingEnabled(true);
+  }
+
+  void SetUp() override {
+    messageQueue = MessageQueue::getInstance();
+    messageQueue->m_messageBuffer.clear();
+    messageQueue->m_filters.clear();
+  }
+
+  void TearDown() override {
+    messageQueue->setSendMessagesAsTasks(false);
+    messageQueue->m_filters.clear();
+    messageQueue->m_messageBuffer.clear();
+  }
+
+  static void TearDownTestSuite() {
+    auto* logManager = LogManager::getInstance().get();
+    logManager->clearLoggers();
+  }
+
+  MessageQueue::Ptr messageQueue;
+};
+
+TEST_F(MessageQueueProcess, goodCase) {
+  TestMessageListener messageListener;
+  ASSERT_EQ(1, messageQueue->m_listeners.size());
+
+  auto message = std::make_shared<TestMessage>();
+  message->setIsLogged(true);
+
+  messageQueue->processMessage(message, false);
+
+  EXPECT_EQ(1, messageListener.m_messageCount);
+}
+
+TEST_F(MessageQueueProcess, goodCaseAsTask) {
+  TaskManager::getScheduler(TabId::app())->startSchedulerLoopThreaded();
+
+  messageQueue->setSendMessagesAsTasks(true);
+
+  TestMessageListener messageListener;
+  ASSERT_EQ(1, messageQueue->m_listeners.size());
+
+  auto message = std::make_shared<TestMessage>();
+  message->setSendAsTask(true);
+  message->setIsLogged(true);
+
+  messageQueue->processMessage(message, true);
+
+  using namespace std::chrono_literals;
+  std::this_thread::sleep_for(50ms);
+
+  EXPECT_EQ(1, messageListener.m_messageCount);
+}
+
+struct MessageQueueSendMessage : testing::Test {
+  static void SetUpTestSuite() {
+    auto* logManager = LogManager::getInstance().get();
+    auto consoleLogger = std::make_shared<ConsoleLogger>();
+    consoleLogger->setLogLevel(Logger::LogLevel::LOG_ALL);
+    logManager->addLogger(consoleLogger);
+    logManager->setLoggingEnabled(true);
+  }
+
+  void SetUp() override {
+    messageQueue = MessageQueue::getInstance();
+  }
+
+  void TearDown() override {}
+
+  static void TearDownTestSuite() {
+    auto* logManager = LogManager::getInstance().get();
+    logManager->clearLoggers();
+  }
+
+  MessageQueue::Ptr messageQueue;
+};
+
+TEST_F(MessageQueueSendMessage, goodCase) {
+  TestMessageListener messageListener;
+  ASSERT_EQ(1, messageQueue->m_listeners.size());
+
+  auto message = std::make_shared<TestMessage>();
+  message->setIsLogged(true);
+
+  messageQueue->sendMessage(message);
+  EXPECT_EQ(1, messageListener.m_messageCount);
+}
+
+struct MessageQueueFilter : testing::Test {
+  static void SetUpTestSuite() {
+    auto* logManager = LogManager::getInstance().get();
+    auto consoleLogger = std::make_shared<ConsoleLogger>();
+    consoleLogger->setLogLevel(Logger::LogLevel::LOG_ALL);
+    logManager->addLogger(consoleLogger);
+    logManager->setLoggingEnabled(true);
+  }
+
+  void SetUp() override {
+    messageQueue = MessageQueue::getInstance();
+  }
+
+  void TearDown() override {}
+
+  static void TearDownTestSuite() {
+    auto* logManager = LogManager::getInstance().get();
+    logManager->clearLoggers();
+  }
+
+  MessageQueue::Ptr messageQueue;
+};
+
+struct MockedMessageFilter : MessageFilter {
+  bool called = false;
+  void filter(MessageQueue::MessageBufferType* /*unused*/) override {
+    called = true;
+  }
+};
+
+TEST_F(MessageQueueFilter, goodCase) {
+  auto filter = std::make_shared<MockedMessageFilter>();
+
+  messageQueue->addMessageFilter(filter);
+  messageQueue->addMessageFilter(filter);
+  messageQueue->addMessageFilter(filter);
+
+  // TODO(Hussein): It should be Eq 1
+  EXPECT_EQ(3, messageQueue->m_filters.size());
 }
