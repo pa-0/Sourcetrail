546a1260a1117df38c1ba67eca58bab3604d561c
Ahmed Abdel Aal <thelastrealdevil@gmail.com>
2024-03-20 06:27:35 +0200

    src: Create GlobalId
    Id is globally used type. It isn't a type. It is using which not recommended. GlobalId will replace the Id. GlobalId is a strong type.

--- src/lib/utility/types.h
+++ src/lib/utility/types.h
@@ -1,5 +1,44 @@
 #pragma once
-// STL
 #include <cstddef>
 
-using Id = size_t;
+using Id = size_t; // TODO(Hussein): Replace it with GlobalId
+
+struct GlobalId final {
+  constexpr GlobalId() noexcept = default;
+  constexpr GlobalId(size_t index) noexcept : mInitialized(false), mValue(index) {}
+
+  constexpr operator bool() const noexcept {
+    return mInitialized;
+  }
+
+  [[nodiscard]] constexpr bool isValid() const noexcept {
+    return mInitialized;
+  }
+
+  constexpr operator size_t() const noexcept {
+    return mValue;
+  }
+
+  [[nodiscard]] constexpr size_t value() const noexcept {
+    return mValue;
+  }
+
+  [[nodiscard]] constexpr bool operator==(GlobalId other) const noexcept {
+    return mInitialized == other.mInitialized && mValue == other.mValue;
+  }
+
+  [[nodiscard]] constexpr bool operator==(Id other) const noexcept {
+    return mValue == other;
+  }
+private:
+  bool mInitialized = false;
+  size_t mValue = 0;
+};
+
+[[nodiscard]] inline constexpr bool operator==(Id other, GlobalId id) noexcept {
+  return id == other;
+}
+
+inline constexpr GlobalId operator"" _gi(unsigned long long int index) noexcept {
+  return GlobalId {index};
+}
